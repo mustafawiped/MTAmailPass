28f8caaaf9cb683e284ad0e7f71e60b32d5e94d2
Mustafa Gur <mustafawiped@gmail.com>
2023-02-18 13:12:19 +0300

    first commit

---
+++ mailyolla.php
@@ -0,0 +1,37 @@
+<?php
+	require_once('class.phpmailer.php');
+    require_once('class.smtp.php');
+    include( "mta_sdk.php" );
+	$input = mta::getInput();
+    $email = $input[0];
+    $code = $input[1];
+    $mail = new PHPMailer();
+ 
+    $mail->IsSMTP();
+    $mail->SMTPDebug = 4;
+    $mail->SMTPAuth = true;
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+    $mail->SMTPSecure = 'tls';
+    $mail->Username = 'maddegamingresmi@gmail.com';
+    $mail->Password = 'taehyungvbambam123';
+    $mail->SetFrom('maddegamingresmi@gmail.com', 'Madde Gaming | Oyuncu Destek Ekibi');
+	$mail->AddReplyTo('maddegamingresmi@gmail.com', 'Madde Gaming | Oyuncu Destek Ekibi');
+	$mail->CharSet = 'UTF-8';
+	
+    $mail->Subject = 'Madde Gaming';
+    $mail->Body    = 'Madde Gaming | Oyuncu Destek Ekibi
+ 
+Selam. Görüyoruz ki şifreni unutmuşsun. Endişelenme sana yardım edeceğiz.
+    
+KURTARMA KODU: '.$code.'
+ 
+Lütfen Yukarıdaki kodu, ('.$code.') Belirtilen yere yazın ardından yeni şifrenizi girin ve kaydet butonuna basın.
+Böylelikle şifrenizi sıfırlamış olacaksınız. 
+
+Saygılarımızla,
+Madde Gaming | Oyuncu Destek Ekibi.
+';
+    $mail->AddAddress($email);
+    $a = $mail->Send();
+?>
